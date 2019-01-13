4b6818520cb83efeb6f8e1e6801a67c74afc18a9
Arianna Choza <unachoza@gmail.com>
2019-01-12 20:57:56 -0800

    ball shakes, button clicks, answers random

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-alert("magic!")
 
 const button8 = document.getElementById("button")
+const answer = document.querySelector(".answer")
 
 
 function magicFunction (){
     console.log("You better believe I was clicked")
+    button.style.visibility = 'hidden'
 }
 
 let userQuestion = 'Will I sleep well tonight?'
@@ -16,26 +17,34 @@ console.log(userQuestion)
 switch(num){
   case 0: 
   console.log('It is certain')
+  answer.innerHTML = "It is certain"
   break;
   case 2:
   console.log('It is decidedly so')
+  answer.innerHTML = "It is decidedly so"
   break;
   case 3:
   console.log('Reply hazy try again')
+  answer.innerHTML = "Reply hazy try again"
   break;
   case 4:
   console.log('Cannot predict now')
+  answer.innerHTML = "Cannot predict now"
   break;
   case 5:
   console.log('nope')
+  answer.innerHTML = "nope"
   break;
 	case 6:
   console.log('My sources say no')
+  answer.innerHTML = "My sources say no"
   break;
   case 7:
   console.log('Outlook not so good')
+  answer.innerHTML = "Outlook not so good"
   break;
   case 1:
   console.log('Signs point to yes')
+  answer.innerHTML = "Signs point to yes"
   break;
 }
