86696867367f9c109a5858eb36080963f1f79eaf
Arianna Choza <unachoza@gmail.com>
2019-01-13 14:00:15 -0800

    ball shakes, click function changes fortune and makes frame1 disappear

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
 
 const button8 = document.getElementById("button")
-const answer = document.querySelector(".answer")
+var displayAnswer = document.querySelector(".displayAnswer")
+var frame1 = document.querySelector("#frame-1")
 
 
 function magicFunction (){
-    console.log("You better believe I was clicked")
+    console.log("Will I have time to rest in between chores?")
     button.style.visibility = 'hidden'
+    frame1.style.visibility = "hidden"
+   
+    switch(num){
+        case 0: 
+        console.log('It is certain')
+        answer = "It is certain"
+        break;
+        case 2:
+        console.log('It is decidedly so')
+        answer = "It is decidedly so"
+        break;
+        case 3:
+        console.log('Reply hazy try again')
+        answer = "Reply hazy try again"
+        break;
+        case 4:
+        console.log('Cannot predict now')
+        answer = "Cannot predict now"
+        break;
+        case 5:
+        console.log('nope')
+        answer = "nope"
+        break;
+          case 6:
+        console.log('My sources say no')
+        answer = "My sources say no"
+        break;
+        case 7:
+        console.log('Outlook not so good')
+        answer = "Outlook not so good"
+        break;
+        case 1:
+        console.log('Signs point to yes')
+        answer = "Signs point to yes"
+        break;
+      }
+    //   answer.innerHTML = (answer)
 }
 
-let userQuestion = 'Will I sleep well tonight?'
+function showAnswer(){
+    displayAnswer.innerHTML = answer
+}
+
+let userQuestion = 'Should I go to the grocery store today?'
 let num =  Math.floor(Math.random() * 7)
 let randomNumber = Math.floor(num)
 let eightBall = ''
 
 console.log(userQuestion)
-switch(num){
-  case 0: 
-  console.log('It is certain')
-  answer.innerHTML = "It is certain"
-  break;
-  case 2:
-  console.log('It is decidedly so')
-  answer.innerHTML = "It is decidedly so"
-  break;
-  case 3:
-  console.log('Reply hazy try again')
-  answer.innerHTML = "Reply hazy try again"
-  break;
-  case 4:
-  console.log('Cannot predict now')
-  answer.innerHTML = "Cannot predict now"
-  break;
-  case 5:
-  console.log('nope')
-  answer.innerHTML = "nope"
-  break;
-	case 6:
-  console.log('My sources say no')
-  answer.innerHTML = "My sources say no"
-  break;
-  case 7:
-  console.log('Outlook not so good')
-  answer.innerHTML = "Outlook not so good"
-  break;
-  case 1:
-  console.log('Signs point to yes')
-  answer.innerHTML = "Signs point to yes"
-  break;
-}
